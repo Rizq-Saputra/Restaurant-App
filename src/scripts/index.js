d5898c238f033c7849ea28744d11f04ae1bad02e
rizq <rizky002211@gmail.com>
2024-03-28 01:03:16 +0800

    Commit Fixing Layout restaurant catalog

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+import 'regenerator-runtime'; /* for async await transpile */
+// import '../styles/main.css';
+// import '../styles/responsive.css';
+import '../styles/style.scss';
+import '../styles/responsives.scss';
+import './view/app.js';
+import '../public/data/DATA.json'
